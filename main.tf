6c0d7e16b2af111d7b9cecd645ade41fe794ce0a
Clemens Wältken <waeltken@users.noreply.github.com>
2021-03-12 15:24:02 +0000

    Add functions app to main.tf

--- main.tf
+++ main.tf
@@ -20,3 +20,14 @@ resource "azurerm_storage_account" "upload" {
   account_tier = "Standard"
   account_replication_type = "LRS"
 }
+
+resource "azurerm_function_app" "upload" {
+  name = "uploadstoragefornewdocs"
+
+  resource_group_name = azurerm_resource_group.devops.name
+  location = local.location
+
+  app_service_plan_id = ""
+}
+
+
