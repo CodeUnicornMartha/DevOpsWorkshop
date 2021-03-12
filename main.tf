8fd002dca0fb9a900f813259e0ddfb6e4153c61b
Clemens Wältken <waeltken@users.noreply.github.com>
2021-03-12 15:19:08 +0000

    Add Storage Account

--- main.tf
+++ main.tf
@@ -2,7 +2,21 @@ provider "azurerm" {
   features {}
 }
 
+locals {
+  location = "westeurope"
+}
+
 resource "azurerm_resource_group" "devops" {
   name     = "devops_workshop"
-  location = "westeurope"
+  location = local.location
+}
+
+resource "azurerm_storage_account" "upload" {
+  name = "uploadstoragefornewdocs"
+
+  location = local.location
+  resource_group_name = azurerm_resource_group.devops.name
+
+  account_tier = "Standard"
+  account_replication_type = "LRS"
 }
