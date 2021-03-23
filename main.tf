a93f2c4cf7ac73f2666bde04e15a5eb7aa7b3f6e
Martha Splitthoff <31500919+CodeUnicornMartha@users.noreply.github.com>
2021-03-23 09:00:06 +0000

    update terraform file

--- main.tf
+++ main.tf
@@ -45,4 +45,55 @@ resource "azurerm_function_app" "upload" {
   app_service_plan_id = azurerm_app_service_plan.function.id
 }
 
+resource "azurerm_function_app" "cognitive_service" {
+  name = "cognitiveservicefunction"
 
+  resource_group_name = azurerm_resource_group.devops.name
+  location = local.location
+    
+  storage_account_name = azurerm_storage_account.upload.name
+  storage_account_access_key = azurerm_storage_account.upload.primary_access_key
+
+
+  app_service_plan_id = azurerm_app_service_plan.function.id
+}
+
+resource "azurerm_cosmosdb_account" "cosmos_db" {
+  name                = "devops-cosmos-db"
+  location            = azurerm_resource_group.devops.location
+  resource_group_name = azurerm_resource_group.devops.name
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+
+  enable_automatic_failover = true
+
+  capabilities {
+    name = "EnableAggregationPipeline"
+  }
+
+  capabilities {
+    name = "mongoEnableDocLevelTTL"
+  }
+
+  capabilities {
+    name = "MongoDBv3.4"
+  }
+
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 10
+    max_staleness_prefix    = 200
+  }
+
+  geo_location {
+    location          = azurerm_resource_group.devops.location
+    failover_priority = 0
+  }
+}
+
+resource "azurerm_cosmosdb_mongo_database" "mongo" {
+  name                = "tfex-cosmos-mongo-db"
+  resource_group_name = azurerm_cosmosdb_account.cosmos_db.resource_group_name
+  account_name        = azurerm_cosmosdb_account.cosmos_db.name
+  throughput          = 400
+}
