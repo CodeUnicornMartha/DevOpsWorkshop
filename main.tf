928dee9246acb3ea9386e928d1af43aa93d2b5a1
Martha Splitthoff <31500919+CodeUnicornMartha@users.noreply.github.com>
2021-03-23 08:42:47 +0000

    update terraform file

--- main.tf
+++ main.tf
@@ -21,13 +21,28 @@ resource "azurerm_storage_account" "upload" {
   account_replication_type = "LRS"
 }
 
+resource "azurerm_app_service_plan" "function" {
+  name                = "function-appservice-plan"
+  location            = azurerm_resource_group.devops.location
+  resource_group_name = azurerm_resource_group.devops.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
 resource "azurerm_function_app" "upload" {
   name = "uploadstoragefornewdocs"
 
   resource_group_name = azurerm_resource_group.devops.name
   location = local.location
+    
+  storage_account_name = azurerm_storage_account.upload.name
+  storage_account_access_key = azurerm_storage_account.upload.primary_access_key
 
-  app_service_plan_id = ""
+
+  app_service_plan_id = azurerm_app_service_plan.function.id
 }
 
 
